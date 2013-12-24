8acaa5791b7e475c2970456938a60e0469ac4eb0
Kevin Rutherford <kevin@rutherford-software.com>
2013-12-24 19:19:13 +0000

    Play a guaranteed winner if we know the oppo's move already

--- bots/delayer.rb
+++ bots/delayer.rb
@@ -6,15 +6,26 @@ class Delayer
   end
 
   def move
+    his = @store.get("#{@name}/oppo_last_move")
+    if his.length > 0
+      my = GameRules.new.moves_that_beat(his, dynamite_allowed?)
+      if my.length > 0
+        return my[0]
+      end
+    end
     legal_moves[@random.rand(legal_moves.length)]
   end
 
   def legal_moves
     result = ['ROCK', 'PAPER', 'SCISSORS']
-    if @store.get('delayer/dynamite_left').to_i > 0
+    if dynamite_allowed?
       result << 'DYNAMITE'
     end
     result
+  end
+
+  def dynamite_allowed?
+    @store.get('delayer/dynamite_left').to_i > 0
   end
 
 end
