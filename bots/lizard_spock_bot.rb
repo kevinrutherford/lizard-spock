b57869261cec63515353e56feaa941c072ad5cfa
Kevin Rutherford <kevin@rutherford-software.com>
2013-12-26 20:51:48 +0000

    Simplify the main bot using the game store

--- bots/lizard_spock_bot.rb
+++ bots/lizard_spock_bot.rb
@@ -3,38 +3,38 @@ class LizardSpockBot
   def initialize(strategy, name, store)
     @strategy = strategy
     @name = name
-    @store = store
+    @game = GameStore.new(name, store)
   end
 
   def start(dynamite_count)
-    @store.set("#{@name}/moves", '')
-    @store.set("#{@name}/dynamite_left", dynamite_count)
-    @store.set("#{@name}/awaiting_oppo", 'false')
+    @game['moves'] = ''
+    @game['dynamite_left'] = dynamite_count
+    @game['awaiting_oppo'] = 'false'
   end
 
   def move
     my_move = @strategy.move
-    @store.set("#{@name}/my_last_move", my_move)
+    @game['my_last_move'] = my_move
     if my_move == 'DYNAMITE'
-      dyn = @store.get("#{@name}/dynamite_left").to_i - 1
-      @store.set("#{@name}/dynamite_left", dyn)
+      dyn = @game['dynamite_left'].to_i - 1
+      @game['dynamite_left'] = dyn
     end
-    history = @store.get("#{@name}/moves") || ''
+    history = @game['moves'] || ''
     history = "#{history}[#{my_move}"
-    @store.set("#{@name}/moves", history)
+    @game['moves'] = history
     my_move
   end
 
   def opponents_move(move)
-    history = @store.get("#{@name}/moves") || ''
+    history = @game['moves'] || ''
     history = "#{history},#{move}]\n"
-    @store.set("#{@name}/oppo_last_move", move)
-    @store.set("#{@name}/moves", history)
-    @store.set("#{@name}/awaiting_oppo", 'false')
+    @game['oppo_last_move'] = move
+    @game['moves'] = history
+    @game['awaiting_oppo'] = 'false'
   end
 
   def game_log
-    @store.get("#{@name}/moves")
+    @game['moves']
   end
 
 end
