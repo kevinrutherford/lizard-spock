2b2a0f799a53ac99bafae52daf60ac84394d24ce
Kevin Rutherford <kevin@rutherford-software.com>
2013-12-29 14:52:07 +0000

    Strip out redis and the strategies to see what's wrong

--- app_launcher.rb
+++ app_launcher.rb
@@ -1,8 +1,4 @@
 require 'sinatra/base'
-require 'redis'
-require_relative 'bots/lizard_spock_bot'
-require_relative 'bots/random_bot'
-require_relative 'bots/delayer'
 
 class LizardSpock < Sinatra::Base
 
@@ -10,63 +6,20 @@ class LizardSpock < Sinatra::Base
     enable :logging
   end
 
-  get '/:bot_name/start' do                         # TESTING ONLY!!!
-    bot(params[:bot_name]).start('fred', 100)
+  post '/start' do
+    @@moves = ''
   end
 
-  post '/:bot_name/start' do
-    bot(params[:bot_name]).start(params['opponentName'], params['dynamiteCount'].to_i)
+  get '/move' do
+    'SCISSORS'
   end
 
-  get '/:bot_name/move' do
-    bot(params[:bot_name]).move
-  end
-
-  post '/:bot_name/move' do
-    bot(params[:bot_name]).opponents_move(params['lastOpponentMove'], params['round'])
-  end
-
-  get '/:bot_name/log' do
-    name = params[:bot_name]
-    "#{bot(name).game_log}"
+  post '/move' do
+    @@moves += "#{params['lastOpponentMove']} (#{params['round']}), "
   end
 
   get '/' do
-    '<a href="/random/log">Random</a></br><a href="/delayer/log">Delayer</a>'
-  end
-
-  #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  def persistent_store
-    return @persistent_store if @persistent_store
-    redisUri = ENV['REDISTOGO_URL'] || 'redis://localhost:6379'
-    uri = URI.parse(redisUri)
-    begin
-      @persistent_store = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-      @persistent_store.keys
-    rescue
-      require_relative 'hash_store'
-      @persistent_store = HashStore.new
-    end
-    return @persistent_store
-  end
-
-  def game(name, persistent_store)
-    Game.new(name, persistent_store)
-  end
-
-  def bot(name)
-    LizardSpockBot.new(strategy(name), game(name, persistent_store))
-  end
-
-  def strategy(name)
-    if @strategies.nil?
-      @strategies = {
-        'random' => RandomBot,
-        'delayer' => Delayer,
-      }
-    end
-    @strategies[name].new
+    @@moves
   end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
