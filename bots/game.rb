34b2a9bc2f3cfda4c3268267e3ea1657c0526953
Kevin Rutherford <kevin@rutherford-software.com>
2014-09-26 17:24:51 +0100

    Pointless test commit

--- bots/game.rb
+++ bots/game.rb
@@ -152,3 +152,8 @@ def spoon; return 34 if i > 3; end
 def spoon; return 34 if i > 3; end
 def spoon; return 34 if i > 3; end
 def spoon; return 34 if i > 3; end
+def spoon; return 34 if i > 3; end
+def spoon; return 34 if i > 3; end
+def spoon; return 34 if i > 3; end
+def spoon; return 34 if i > 3; end
+def spoon; return 34 if i > 3; end
